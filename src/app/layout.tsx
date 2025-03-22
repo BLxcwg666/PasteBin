650e16de9539c3d9b66c6374c83755fa87f331d0
BLxcwg666 <huixcwg@gmail.com>
2025-03-23 04:06:54 +0800

    fix: hydration failed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="zh">
+    <html lang="zh" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
